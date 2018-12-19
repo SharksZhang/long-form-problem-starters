b3b4bb44be6a480975f4e530fe8049628170f231
SharksZhang <2425194544@qq.com>
2018-12-19 13:44:06 +0800

    CoffeeMaker:step3, add NotEhoughBeansException and TooManyBeansException

--- CoffeeMaker/src/exceptions/BeansAmountException.java
+++ CoffeeMaker/src/exceptions/BeansAmountException.java
@@ -1,10 +1,15 @@
-package model;
+package exceptions;
 
 public class BeansAmountException extends Exception {
     private double beans;
 
     public BeansAmountException(double beans){
         super(beans + "is not the right amount of beans");
+        this.beans = beans;
     }
     public double getBeans(){return this.beans;}
+    protected BeansAmountException(double beans, String message){
+        super(beans + message);
+        this.beans = beans;
+    }
 }
