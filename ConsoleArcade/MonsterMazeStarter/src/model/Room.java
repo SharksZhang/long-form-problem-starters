d21c6e33a750368efe926c64763b88057685bff1
SharksZhang <2425194544@qq.com>
2019-01-11 15:15:37 +0800

    MonsterMaze complete

--- ConsoleArcade/MonsterMazeStarter/src/model/Room.java
+++ ConsoleArcade/MonsterMazeStarter/src/model/Room.java
@@ -3,20 +3,15 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class  Room {
+public class  Room extends Choice {
 
-    private List<Monster> monsters;
-    private List<Treasure> treasures;
-    private List<Room> rooms;
+    private List<Choice> choices;
     private int id;
-    private String optionMessage;
 
     public Room(int id) {
-        optionMessage = "Enter Room " + id + ".";
+        super( "Enter Room " + id + ".");
         this.id = id;
-        monsters = new ArrayList<>();
-        treasures = new ArrayList<>();
-        rooms = new ArrayList<>();
+        choices = new ArrayList<>();
     }
 
     //EFFECTS: prints a message representing this possible next choice
@@ -25,72 +20,30 @@ public class  Room {
     }
 
     //EFFECTS: prints all possible next choices
-    public void printNextChoices() {
+    public void printOutcome() {
         System.out.println("You are now in Room " + id + ".\n");
         System.out.println("You have the following options: ");
 
-        int counter = 1;
-
-        for (Monster m: monsters) {
-            System.out.print("\tOption " + counter + ": ");
-            m.printOptionMessage();
-            counter++;
+        for (int i = 0; i< choices.size(); i ++) {
+            int j = i +1;
+            System.out.print("\tOption " + j + ": ");
+            choices.get(i).printOptionMessage();
         }
 
-        for (Treasure t: treasures) {
-            System.out.print("\tOption " + counter + ": ");
-            t.printOptionMessage();
-            counter++;
-        }
-
-        for (Room r: rooms) {
-            System.out.print("\tOption " + counter + ": ");
-            r.printOptionMessage();
-            counter++;
-        }
     }
 
-    //MODIFIES: this
-    //EFFECTS: adds m to next possible monsters
-    public void addMonster(Monster m) {
-        monsters.add(m);
+    public List<Choice> getChoices() {
+        return  choices;
     }
 
-    //MODIFIES: this
-    //EFFECTS: adds t to next possible treasures
-    public void addTreasure(Treasure t) {
-        treasures.add(t);
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
     }
 
-    //MODIFIES: this
-    //EFFECTS: adds r to next possible rooms
-    public void addRoom(Room r) {
-        rooms.add(r);
+    public Choice getChoice(int i){
+        return choices.get(i - 1);
     }
-
-    //getters for gameplay
-    public Monster getMonster(int i) {
-        return monsters.get(i);
+    public void addChoice(Choice choice){
+         choices.add(choice);
     }
-
-    public Treasure getTreasure(int i) {
-        return treasures.get(i);
-    }
-
-    public Room getRoom(int i) {
-        return rooms.get(i);
-    }
-
-    public int getMonsterRange() {
-        return monsters.size();
-    }
-
-    public int getTreasureRange() {
-        return treasures.size();
-    }
-
-    public int getRoomRange() {
-        return rooms.size();
-    }
-
 }
